21ff2c021f44660934c010688eaa0f42ce3424d8
Stephany Camacho-Lara <stephany_c19@yahoo.com>
2019-09-27 15:12:52 -0600

    password is generating in console log

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var bigChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var smallChar = 'abcdefghijklmnopqrstuvwxyz';
-var numbers = 0123456789;
-var specialChar = '~!@#$%^&*()_+=';
+var upperEl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerEl = 'abcdefghijklmnopqrstuvwxyz';
+var numEl = '0123456789';
+var specialEl = '~!@#$%^&*()_+=';
 
 var charLength = prompt("How many characters?");
 var numChar = confirm("Would you like numbers?");
 var lowerChar = confirm("Would you like lowercase characters?");
 var upperChar = confirm("Would you like uppercase characters?");
+var special = confirm("Would you like special characters?");
 
-var generatePassword = function (charLength){
-    
-    retValue = "";
-    for(var i = 0; i<= charLength; i++){
-        var i = Math.floor(Math.random() * charLength);
+function generatePassword() {
+    var result = '';
+    for (var i = 0; i < charLength; i++) {
+      result += upperEl.charAt(Math.floor(Math.random() * upperEl.length));
     }
-    return retValue;
-    document.getElementById("password");
-}
+    return result;
+  }
+  console.log(generatePassword(charLength));
